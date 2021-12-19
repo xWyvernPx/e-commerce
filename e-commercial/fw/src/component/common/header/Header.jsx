61dbf7acf494c0a20cfff191dfc1719a5171a699
xWyvernPx <phonglethanh2@gmail.com>
2021-12-19 20:27:55 +0700

    done filter product page

--- e-commercial/fw/src/component/common/header/Header.jsx
+++ e-commercial/fw/src/component/common/header/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeaderTop from './HeaderTop'
 import "./header.scss"
 import HeaderMiddle from './HeaderMiddle'
 import HeaderBottom from './HeaderBottom'
+import { useDispatch } from 'react-redux'
+import { loadList } from '../../../feature/Landing/landingSlice'
 function Header() {
+    const dispatch = useDispatch();
+     useEffect(() => {
+        dispatch(loadList());
+     }, [dispatch])
     return (
         <header className="header">
             <HeaderTop></HeaderTop>
